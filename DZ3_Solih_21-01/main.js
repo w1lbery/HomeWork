722aa07a057f5310581724cdfef0cb665fd5e05e
w1lbery <116970588+w1lbery@users.noreply.github.com>
2022-12-13 21:14:24 +0600

    DZ3

---
+++ DZ3_Solih_21-01/main.js
@@ -0,0 +1,53 @@
+function minNumber(a, b) {
+    Number(a,b)
+    if (a<b) {
+        console.log(a);
+    }
+    else if (a === b){
+        console.log("===")
+    }
+    else {
+        console.log(b);
+    }
+}
+minNumber(10,2)
+
+const userString = prompt("Enter String")
+
+function getStringLenght(string){
+    return console.log(string.length);
+}
+
+getStringLenght(userString)
+
+// function calc(num1, num2){
+//     Number(num1, num2)
+//
+// }
+
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+const number1 = Number(prompt('Enter first number: '));
+const number2 = Number(prompt('Enter second number: '));
+
+let result;
+
+function calc(){
+
+    if (operator === '+') {
+        result = number1 + number2;
+    }
+    else if (operator === '-') {
+        result = number1 - number2;
+    }
+    else if (operator === '*') {
+        result = number1 * number2;
+    }
+    else {
+        result = number1 / number2;
+    }
+    return  console.log(`${number1} ${operator} ${number2} = ${result}`);
+
+}
+calc()
+
